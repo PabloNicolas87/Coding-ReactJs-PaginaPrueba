4a6ba9f9be8109ced809624c44bd456dfcd5d859
PabloNicolas87 <gironepablo@gmail.com>
2025-07-02 15:13:37 -0300

    Configura Vitest para incluir tests en src/tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: 'jsdom',
-  },
+    setupFiles: './src/tests/setupTests.ts',
+    globals: true,
+    include: ['src/tests/**/*.test.tsx']
+  }
 });
